504b86713ba5652201d14cf35106012b254c1c64
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 21:40:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,18 +10,11 @@ class Triangle
     @all << @length1
     @all << @length2
     @all << @length3
-    # @x=@all[0]
-    # @y=@all[1]
-    # @z=@all[2]
   end 
   
   def all_equal?
     @all.uniq.size <=1 
   end 
-  
-  # def two_equal?(arr)
-  #   @length1==@length2 || @length1==@length3 || @length2==@length3
-  # end 
   
   def validator
     if (@length1==0 && @length2==0 && @length3==0) || (@length1.negative?() || @length2.negative?() || @length3.negative?() ) || (@length1+@length2 <@length3 || @length2+@length3 <@length1 || @length1+@length3 <@length2)
